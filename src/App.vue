88a93602381a9473981a555106c6266ce40067f5
Alexandre <alexandre.prigent2910@gmail.com>
2025-03-21 08:40:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import CardView from './components/CardView.vue';
+import { userWatchList } from './assets/static/js/watchList.js';
+const localSeries = ref(userWatchList);
+</script>
+
+<template>
+    <div>
+        <div class="container">
+            <section class="sidebar">
+                <ul>
+                    <li>Ici c'est fixe</li>
+                    <li>Un lien de nav 1</li>
+                    <li>Un lien de nav 2</li>
+                </ul>
+                <span>▶ Lecture en cours: (ep title) </span>
+            </section>
+            <main class="courses-container">
+                <!-- v-for permet de faire un element html par objet dans mon tableau d'objet -->
+                <CardView v-for="serie in localSeries" :series="serie.series" :title="serie.title"
+                    :episode="serie.episode" :season="serie.season" />
+            </main>
+        </div>
+    </div>
+
+
+
+</template>
+
+<style scoped></style>
---
+++ src/components/CardView.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    series: String,
+    season: Number,
+    episode: Number,
+    title: String,
+    timeCursor: String,
+})
+</script>
+<template>
+    <div class="watch-card">
+        <div class="course-preview">
+            <h6>{{ series }}</h6>
+            <h2>{{ title }}</h2>
+        </div>
+        <div class="course-info">
+            <h6>Season{{ season }} x episode{{ episode }}</h6>
+            <h2>{{ title }}</h2>
+            <button class="btn">Continue</button>
+        </div>
+    </div>
+</template>
